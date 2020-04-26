7d76c4fbd103252c9e69f38cca76bf609033c2bd
Mohammad Nazir <unzilaeman@hotmail.com>
2020-04-26 07:17:56 +0530

    chat html added

---
+++ PubSubApp/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PubSubApp.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public string UserID { get; set; }
+        public string SocketId { get; set; }
+
+    }
+}
--- PubSubApp/Startup.cs
+++ PubSubApp/Startup.cs
@@ -34,8 +34,8 @@ namespace PubSubApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(option => { option.EnableEndpointRouting = false; })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddLogging(builder =>
             {
                 builder.AddConsole()
